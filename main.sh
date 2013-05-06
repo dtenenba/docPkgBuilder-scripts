4f181a3eb925b88ffd2b902e55d0fd1fbf9145ad
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-05-06 13:24:26 -0700

    better logging, do all steps

--- main.sh
+++ main.sh
@@ -15,21 +15,15 @@ mkdir $CHECK_DIR
 
 rm -f $WORKSPACE/*.tar.gz
 $HOME_OF_R/bin/Rscript $BUILDER_SCRIPTS/docPkgBuilder-scripts/prerun.R
-echo "d0"
 echo "workspace is $WORKSPACE"
-echo "current dir is"
-echo `pwd`
 cd $WORKSPACE
-echo "now current dir is"
-echo `pwd`
-# uncomment this:
-###$HOME_OF_R/bin/R CMD build $WORKSPACE
+echo ">>> Running R CMD build:"
+$HOME_OF_R/bin/R CMD build $WORKSPACE
 # remove this:
-$HOME_OF_R/bin/R CMD build --no-vignettes $WORKSPACE
-echo "d1"
-# uncomment this:
-# cd $CHECK_DIR
-###$HOME_OF_R/bin/R CMD check --no-vignettes $WORKSPACE/*.tar.gz
+#$HOME_OF_R/bin/R CMD build --no-vignettes $WORKSPACE
+cd $CHECK_DIR
+echo ">>> Running R CMD check:"
+$HOME_OF_R/bin/R CMD check --no-vignettes $WORKSPACE/*.tar.gz
 # rm -rf $CHECK_DIR
 
 if [ "$NODE_NAME" = "master" ]; then
@@ -38,16 +32,13 @@ if [ "$NODE_NAME" = "master" ]; then
     rm -rf library
     mkdir library
     #R_LIBS_USER=$WORKSPACE/library 
+    echo ">>> Running R CMD INSTALL..."
     $HOME_OF_R/bin/R CMD INSTALL --library=library $WORKSPACE/*.tar.gz
 
     tar zxf *.tar.gz
     export pkg=`echo *.tar.gz | cut -d _ -f 1`
-    echo "pkg is:"
-    echo $pkg
     rm -rf $pkg.Rcheck
     cd $pkg
-    echo "current directory:"
-    echo `pwd`
     if [ -d "vignettes" ]; then
         cd vignettes
     else
@@ -55,16 +46,16 @@ if [ "$NODE_NAME" = "master" ]; then
         then
             cd inst/doc
         else
-            echo "There are no vignettes in this package!"
+            echo ">>> ERROR: There are no vignettes in this package!"
             exit 1
         fi
     fi
     for i in `ls *.Rmd`
     do
         #R_LIBS_USER=$WORKSPACE/library 
-        echo "Building $i..."
-        $HOME_OF_R/bin/R --vanilla -q -e "library(knitr);.libPaths(c(file.path(Sys.getenv('WORKSPACE'),'library'), .libPaths()));.libPaths();knit('$i')"
+        echo ">>> Building $i..."
+        $HOME_OF_R/bin/R --vanilla -q -e "library(knitr);.libPaths(c(file.path(Sys.getenv('WORKSPACE'),'library'), .libPaths()));knit('$i')"
     done
     tar zcf $WORKSPACE/$pkg-vignettes.tar.gz .
-    echo "Vignette tarball has been created."
+    echo ">>> Vignette tarball has been created."
 fi
