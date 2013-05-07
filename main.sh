2409759d74f55254fd6228e7f2b3aa87d60e9dc8
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-05-07 11:54:50 -0700

    remove cruft, try building with vignettes

--- main.sh
+++ main.sh
@@ -2,9 +2,6 @@
 
 set -e  # Exit immediately if a simple command exits with a non-zero status
 
-echo "TEMP = $TEMP"
-echo "TMP = $TMP"
-
 if [ -z "$TMP" ]; then
     TMP=/tmp
 fi
@@ -13,24 +10,19 @@ CHECK_DIR=$TMP/$BUILD_TAG
 mkdir $CHECK_DIR
 
 cd $WORKSPACE
-echo "JOB_NAME IS $JOB_NAME"
 rm -rf `echo $JOB_NAME|cut -d / -f 1`
 
 rm -f $WORKSPACE/*.tar.gz
 $HOME_OF_R/bin/Rscript $BUILDER_SCRIPTS/docPkgBuilder-scripts/prerun.R
 echo "workspace is $WORKSPACE"
 echo ">>> Running R CMD build:"
-#$HOME_OF_R/bin/R CMD build $WORKSPACE
+$HOME_OF_R/bin/R CMD build $WORKSPACE
 # remove this:
-$HOME_OF_R/bin/R CMD build --no-vignettes $WORKSPACE
-echo "about to cd to check dir"
+#$HOME_OF_R/bin/R CMD build --no-vignettes $WORKSPACE
 cd $CHECK_DIR
-echo "should be in check dir"
 ##echo ">>> Running R CMD check:"
 ##$HOME_OF_R/bin/R CMD check --no-vignettes $WORKSPACE/*.tar.gz
-echo "about to cd to workspace"
 cd $WORKSPACE
-echo "should be in workspace"
 rm -rf $CHECK_DIR
 
 if [ "$NODE_NAME" = "master" ]; then
--- make-vignettes.sh
+++ make-vignettes.sh
@@ -7,8 +7,6 @@ killall Xvfb || true
 killall Xvfb || true
 echo "killed those processes, now starting X"
 . /var/lib/jenkins/start-virtual-X.sh > /dev/null 2>&1
-# > /dev/null 2>&1 &
-echo "started X"
     
 echo "workspace is $WORKSPACE"
 cd $WORKSPACE
