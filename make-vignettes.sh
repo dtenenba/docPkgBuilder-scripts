b6aab13a9aefa111cb57cb981b66960cfbde1ff1
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-05-07 11:47:06 -0700

    see output of script that starts X

--- make-vignettes.sh
+++ make-vignettes.sh
@@ -6,7 +6,8 @@ killall Xvfb || true
 killall Xvfb || true
 killall Xvfb || true
 echo "killed those processes, now starting X"
-. /var/lib/jenkins/start-virtual-X.sh > /dev/null 2>&1 &
+. /var/lib/jenkins/start-virtual-X.sh
+# > /dev/null 2>&1 &
 echo "started X"
     
 echo "workspace is $WORKSPACE"
